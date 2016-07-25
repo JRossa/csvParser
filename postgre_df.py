b9d77af6515d8fe4416a13fe03f824b5aa078067
JRossa <rossa.jmr@gmail.com>
2016-07-25 15:38:12 +0100

    postges_df.gf changed to 'select exists'

--- postgre_df.py
+++ postgre_df.py
@@ -115,7 +115,11 @@ class postgresDB:
 		grantPostGresStatement = "GRANT ALL ON TABLE public.ft_" + ft_tableName + " TO postgres;"
 		grantPublicStatement = "GRANT SELECT ON TABLE public.ft_" + ft_tableName + " TO public;"
 
-		ckeckStatement = 'SELECT to_regclass(\'public.ft_' + ft_tableName + '\')'
+#		ckeckStatement = 'SELECT to_regclass(\'public.ft_' + ft_tableName + '\')'
+		ckeckStatement = 'SELECT EXISTS (SELECT 1 FROM   information_schema.tables ' \
+   							'WHERE  table_schema = "public" '\
+   							'AND table_name = ' + ft_tableName + ')'
+
 		print ckeckStatement
 
 		cursor = self.conn.cursor()
@@ -123,7 +127,8 @@ class postgresDB:
 
 		rSet = cursor.fetchall()
 		print rSet[0][0]
-
+		exit(0)
+		
 		if str(rSet[0][0]) == 'None':
 			cursor.close()
 
